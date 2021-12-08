dcf728b47763a84ddc351219e146311816a99673
JE-Chen <zenxcvwait@gmail.com>
2021-12-08 20:24:52 +0800

    add new repo and init
    init file
    add repo

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as README:
+    long_description = README.read()
+
+setuptools.setup(
+    name="load_testing_je",
+    version="0.0.01",
+    author="JE-Chen",
+    author_email="zenmailman@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JE-Chen/LoadTesting",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "locust"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
+
+# python setup.py sdist bdist_wheel
+# python -m twine upload dist/*
