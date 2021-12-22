8f63e5d276a84d4ca831866d6cef87becea68939
JE-Chen <zenxcvwait@gmail.com>
2021-12-22 20:04:09 +0800

    add exception & add path check  & refactor
    refactor requirements.txt
    add exception on utils

--- test/start_locust_test/start_locust_test.py
+++ test/start_locust_test/start_locust_test.py
@@ -1,4 +1,4 @@
-
+import shutil
 import time
 
 from load_testing_je import start_locust
@@ -9,5 +9,5 @@ from load_testing_je import change_to_locust_file_dir
 if __name__ == "__main__":
     change_to_locust_file_dir(r"D:\WorkSpaces\Program WorkSpace\Python\Project\LoadTesting\test\basic_test")
     locust_process = start_locust()
-    time.sleep(10)
+    time.sleep(5)
     stop_locust(locust_process)
