a0ff0ac76a05cc057171a33dc3416312b792f968
JE-Chen <zenxcvwait@gmail.com>
2022-04-17 23:04:05 +0800

    add web ui default host and port
    add web ui default host and port
    add catch response and need to edit

--- je_load_testing/wrapper/locust_template/http_user_with_api_testka.py
+++ je_load_testing/wrapper/locust_template/http_user_with_api_testka.py
@@ -9,21 +9,22 @@ def create_loading_test_user(user_detail_dict: dict, **kwargs):
     :param user_detail_dict: detail_dict should be included host http_method test_path
     another_test_setting_dict are optional
     """
-    http_method = user_detail_dict.get("request_method")
-    request_url = user_detail_dict.get("request_url")
+    http_method = user_detail_dict.get("request_method", "get")
+    request_url = user_detail_dict.get("request_url", "http://localhost")
     another_test_setting_dict = user_detail_dict.get("another_test_setting_dict", None)
+    assert_result_dict = user_detail_dict.get("assert_result_dict", None)
     loading_test_detail_dict.update(
         {
             "http_method": http_method,
             "request_url": request_url,
-            "another_test_setting_dict": another_test_setting_dict
+            "another_test_setting_dict": another_test_setting_dict,
+            "assert_result_dict": assert_result_dict
         }
     )
     return HttpUserWrapper
 
 
 class HttpUserWrapper(HttpUser):
-
     host = ""
     min_wait = 5
     max_wait = 20
@@ -43,11 +44,28 @@ class HttpUserWrapper(HttpUser):
 
     @task
     def task_with_api_testka(self):
-        if self.loading_test_detail_dict.get("another_test_setting_dict") is not None:
-            self.test_client(
-                self.loading_test_detail_dict.get("request_url"),
-                **self.loading_test_detail_dict.get("another_test_setting_dict")
-            )
+        another_test_setting_dict: dict = self.loading_test_detail_dict.get("another_test_setting_dict")
+        assert_result_dict: dict = self.loading_test_detail_dict.get("assert_result_dict")
+        if another_test_setting_dict is not None:
+            if assert_result_dict is None:
+                self.test_client(
+                    self.loading_test_detail_dict.get("request_url"),
+                    **self.loading_test_detail_dict.get("another_test_setting_dict")
+                )
+            else:
+                with self.test_client(
+                        self.loading_test_detail_dict.get("request_url"),
+                        **self.loading_test_detail_dict.get("another_test_setting_dict"),
+                        catch_response=True
+                ) as response:
+                    pass
         else:
-            self.test_client(self.loading_test_detail_dict.get("request_url"))
-
+            if assert_result_dict is None:
+                self.test_client(self.loading_test_detail_dict.get("request_url"))
+            else:
+                with self.test_client(
+                        self.loading_test_detail_dict.get("request_url"),
+                        **self.loading_test_detail_dict.get("another_test_setting_dict"),
+                        catch_response=True
+                ) as response:
+                    pass
