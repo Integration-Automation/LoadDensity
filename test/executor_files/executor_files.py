b87931d4a1422ccf0381bc19ccc1d8f4bcfe16b8
JE-Chen <zenxcvwait@gmail.com>
2022-04-20 23:39:22 +0800

    update stable version
    update stable version

--- test/executor_files/executor_files.py
+++ test/executor_files/executor_files.py
@@ -2,6 +2,5 @@ from je_locust_wrapper import execute_files
 from je_locust_wrapper import get_dir_files_as_list
 
 files_list = get_dir_files_as_list("/test/executor_files")
-print(files_list)
 
 execute_files(files_list)
