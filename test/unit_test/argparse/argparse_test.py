8b5372003f555eb3e995a8b2127eb1ef6a4cec65
JE-Chen <zenxcvwait@gmail.com>
2022-07-06 15:34:53 +0800

    refactor __main__
    refactor __main__

--- test/unit_test/argparse/argparse_test.py
+++ test/unit_test/argparse/argparse_test.py
@@ -6,6 +6,6 @@ print(os.getcwd())
 use os system to test argparse 
 """
 os.system("cd " + os.getcwd())
-os.system("python je_load_density --execute_file " + os.getcwd() + r"/test/argparse/test_json_file1.json")
-os.system("python je_load_density --execute_dir " + os.getcwd() + r"/test/argparse")
+os.system("python je_load_density --execute_file " + os.getcwd() + r"/test/unit_test/argparse/test_json_file1.json")
+os.system("python je_load_density --execute_dir " + os.getcwd() + r"/test/unit_test/argparse")
 
