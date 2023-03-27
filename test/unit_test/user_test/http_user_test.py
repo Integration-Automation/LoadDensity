d613a46221a3d13604d08ef0efa3d1f90718df80
JE-Chen <zenxcvwait@gmail.com>
2023-03-27 16:47:14 +0800

    fix and test error

--- test/unit_test/user_test/http_user_test.py
+++ test/unit_test/user_test/http_user_test.py
@@ -5,7 +5,7 @@ start_test(
         "user": "http_user",
     },
     50, 10, 5,
-    **{
+    tasks={
         "get": {"request_url": "http://httpbin.org/get"},
         "post": {"request_url": "http://httpbin.org/post"}
     }
