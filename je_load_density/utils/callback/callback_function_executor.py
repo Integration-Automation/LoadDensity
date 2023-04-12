48835918bb4b11663fba8c3b8b9b5b121e1ec583
JE-Chen <zenxcvwait@gmail.com>
2023-04-12 22:41:24 +0800

    Add getting started doc and installation doc
    Add getting started doc and installation doc

--- je_load_density/utils/callback/callback_function_executor.py
+++ je_load_density/utils/callback/callback_function_executor.py
@@ -48,7 +48,7 @@ class CallbackFunctionExecutor(object):
                 if callback_param_method == "kwargs":
                     callback_function(**callback_function_param)
                 else:
-                    callback_function(*callback_function_param.values())
+                    callback_function(*callback_function_param)
             else:
                 callback_function()
             return execute_return_value
