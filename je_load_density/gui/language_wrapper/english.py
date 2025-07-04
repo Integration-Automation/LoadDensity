c87c4588f078e70414ba6e6a4b09a3418873e088
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2025-07-05 01:03:27 +0800

    Update dev version
    Update dev version
    - Add GUI

---
+++ je_load_density/gui/language_wrapper/english.py
@@ -0,0 +1,20 @@
+english_word_dict = {
+    # Main
+    "application_name": "LoadDensity",
+    # Widget
+    "url": "URL",
+    "test_time": "Test Time: ",
+    "user_count": "User Count: ",
+    "spawn_rate": "Spawn Rate: ",
+    "test_method": "Test Method: ",
+    "start_button": "Start: ",
+    "log": "Log: ",
+    # Test method
+    "get": "GET",
+    "post": "POST",
+    "put": "PUT",
+    "patch": "PATCH",
+    "delete": "DELETE",
+    "head": "HEAD",
+    "options": "OPTIONS",
+}
