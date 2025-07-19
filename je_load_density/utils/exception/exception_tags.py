baf77aa4742c9e8b7cef07e671f520605431eeee
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2025-07-19 16:25:06 +0800

    Update both version
    Update exception tags

--- je_load_density/utils/exception/exception_tags.py
+++ je_load_density/utils/exception/exception_tags.py
@@ -1,23 +1,30 @@
 # locust error
-not_found_locust_error: str = "locust not found"
+not_found_locust_error: str = "Locust not found"
+
 # json error
-cant_reformat_json_error: str = "can't reformat json is type right?"
-wrong_json_data_error: str = "can't parser json"
-cant_find_json_error: str = "can't find json"
-cant_save_json_error: str = "can't save json"
-cant_generate_json_report: str = "can't generate json report"
+cant_reformat_json_error: str = "can't reformat JSON: is the type correct?"
+wrong_json_data_error: str = "can't parse JSON"
+cant_find_json_error: str = "can't find JSON file"
+cant_save_json_error: str = "can't save JSON file"
+cant_generate_json_report: str = "can't generate JSON report"
+
 # executor error
-executor_data_error: str = "executor receive wrong data"
-executor_list_error: str = "executor receive wrong data list is none or wrong type"
+executor_data_error: str = "executor received invalid data"
+executor_list_error: str = "executor received invalid data: list is empty or wrong type"
+
 # HTML
-html_generate_no_data_tag: str = "record is None"
+html_generate_no_data_tag: str = "no records to generate HTML report"
+
 # add command
-add_command_exception_tag: str = "command value type should be as method or function"
+add_command_exception_tag: str = "command value type must be a method or function"
+
 # argparse
-argparse_get_wrong_data: str = "argparse receive wrong data"
+argparse_get_wrong_data: str = "argparse received invalid data"
+
 # XML
-cant_read_xml_error: str = "can't read xml"
-xml_type_error: str = "xml type error"
+cant_read_xml_error: str = "can't read XML"
+xml_type_error: str = "XML type error"
+
 # Callback executor
-get_bad_trigger_method: str = "get bad trigger method, only accept kwargs and args"
-get_bad_trigger_function: str = "get bad trigger function only accept function in event_dict"
+get_bad_trigger_method: str = "invalid trigger method: only kwargs and args accepted"
+get_bad_trigger_function: str = "invalid trigger function: only functions in event_dict accepted"
