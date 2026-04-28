76b5100f0d89d772ff70ee4bf17fd72f2b15fabb
JeffreyChen <zenxcvwait@gmail.com>
2026-04-28 20:51:14 +0800

    feat: add HAR record/replay importer
    har_to_tasks turns a HAR log into a list of LoadDensity HTTP tasks
    and har_to_action_json wraps it as a runnable action JSON. Filters
    entries by include/exclude regex over URL, strips hop-by-hop and
    HTTP/2 pseudo headers, decodes JSON bodies into the json field, and
    preserves the original status code as a status_code assertion.

---
+++ je_load_density/utils/recording/__init__.py
@@ -0,0 +1,7 @@
+from je_load_density.utils.recording.har_importer import (
+    har_to_action_json,
+    har_to_tasks,
+    load_har,
+)
+
+__all__ = ["har_to_action_json", "har_to_tasks", "load_har"]
